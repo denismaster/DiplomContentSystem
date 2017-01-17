a2e856b0024199c7dcf281a734d2587191fab038
Denis Obolensky <denismaster@outlook.com>
2017-01-17 15:09:12 +0300

    DCS-2: Using FileSaver to simplifying saving data.

--- src/DiplomContentSystem/ClientApp/app/teacher/components/teacher.component.ts
+++ src/DiplomContentSystem/ClientApp/app/teacher/components/teacher.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-
+import { Http, Response } from '@angular/http';
+import * as FileSaver from "file-saver";
 @Component({
     selector: 'teachers',
     templateUrl: './teacher.component.html'
@@ -8,11 +8,24 @@ import { Http } from '@angular/http';
 export class TeachersComponent {
     public teachers: Teacher[];
 
-    constructor(http: Http) {
-        http.get('/api/teachers').subscribe(result => {
+    constructor(private http: Http) {
+        this.http.get('/api/teachers').subscribe(result => {
             let teachers = result.json() as Teacher[];
             this.teachers = teachers;
         });
+    }
+    sendRequest() {
+        let object = {
+            "id": 1,
+            "data": [
+                "\\documentclass{article}\n\\begin{document}\nHello,Denismaster!\n\\end{document}\n"]
+        };
+        this.http.get("http://localhost:1337/api/sample").subscribe(data => this.downloadFile(data));
+    }
+    downloadFile(data: Response) {
+        let blob = new Blob([data], { type: 'application/pdf' });
+        let url = window.URL.createObjectURL(blob);
+        FileSaver.saveAs(blob,"sample.pdf");
     }
 }
 
