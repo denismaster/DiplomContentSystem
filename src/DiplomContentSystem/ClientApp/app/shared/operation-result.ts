51cb109482f56231786bef205771b746c37c0f1f
Denis Obolensky <denismaster@outlook.com>
2017-01-31 11:34:46 +0300

    DCS-4: Custom validators functions. OperationResult. Some changes to models

---
+++ src/DiplomContentSystem/ClientApp/app/shared/operation-result.ts
@@ -0,0 +1,5 @@
+export class OperationResult
+{
+    public hasErrors:boolean = false;
+    public errors:string[]=undefined;
+}
---
+++ src/DiplomContentSystem/ClientApp/app/teacher/models/teacher.ts
@@ -0,0 +1,6 @@
+export class Teacher {
+    id: number;
+    fio: string;
+    position: string;
+    maxWorkCount: string;
+}
