5c28cdf9100abe7e1e03194e4d77973ed01566fe
Denis Obolensky <denismaster@outlook.com>
2017-01-31 11:37:12 +0300

    DCS-4: Frontend Teacher Add implementation

--- src/DiplomContentSystem/ClientApp/app/teacher/teacher.service.ts
+++ src/DiplomContentSystem/ClientApp/app/teacher/teacher.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Teacher } from './components/teacher.component';
+import { Teacher } from './models/teacher';
 import { ServiceBase } from '../shared/service-base';
+import { OperationResult } from '../shared/operation-result';
 
 @Injectable()
 export class TeacherService extends ServiceBase{
     constructor(private http: Http)
     {
         super();
+    }
+
+    public addTeacher(teacher:Teacher):Observable<OperationResult>
+    {
+        console.log(teacher);
+        return this.http.post('/api/teachers/add',teacher).map(this.extractData);
     }
 
     public getTeachers():Observable<Teacher[]>
