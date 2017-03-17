e729344d4e088e269552e0aed2aa6d4f328e39eb
Denis Obolensky <denis_obolensky@xrm.ru>
2017-03-17 17:33:22 +0300

    DCS-10: Changes to MappingProfile.

--- src/DiplomContentSystem.Services/MappingProfile.cs
+++ src/DiplomContentSystem.Services/MappingProfile.cs
@@ -14,6 +14,12 @@ namespace DiplomContentSystem.Services
             CreateMap<Teacher, TeacherListItem>()
                 .ForMember(item => item.Speciality, opt => opt.MapFrom(src => (src.Speciality!=null)?src.Speciality.ShortName:null))
                 .ForMember(item => item.Position, opt => opt.MapFrom(src => (src.Position!=null)?src.Position.Name:null));
+            CreateMap<TeacherEditItem, Teacher>()
+                .ForMember(item=>item.Id, opt=>opt.MapFrom(src=>src.Id==null?0:src.Id))
+                .ForMember(item=>item.PositionId,opt=>opt.MapFrom(src=>src.PositionId))
+                .ForMember(item=>item.Position, opt=>opt.Ignore())
+                .ForMember(item=>item.Speciality, opt=>opt.Ignore())
+                .ForMember(item=>item.SpecialityId, opt=>opt.UseValue(1));
         }
     }
 }
