27c857164b4154bb990a0ece6b3d39b58ef5e5db
Denis Obolensky <denis_obolensky@xrm.ru>
2017-03-18 14:12:57 +0300

    DCS-10: Small fix commit

--- src/DiplomContentSystem.Services/MappingProfile.cs
+++ src/DiplomContentSystem.Services/MappingProfile.cs
@@ -17,9 +17,9 @@ namespace DiplomContentSystem.Services
             CreateMap<TeacherEditItem, Teacher>()
                 .ForMember(item=>item.Id, opt=>opt.MapFrom(src=>src.Id==null?0:src.Id))
                 .ForMember(item=>item.PositionId,opt=>opt.MapFrom(src=>src.PositionId))
+                .ForMember(item=>item.SpecialityId, opt=>opt.UseValue(1))
                 .ForMember(item=>item.Position, opt=>opt.Ignore())
-                .ForMember(item=>item.Speciality, opt=>opt.Ignore())
-                .ForMember(item=>item.SpecialityId, opt=>opt.UseValue(1));
+                .ForMember(item=>item.Speciality, opt=>opt.Ignore());
         }
     }
 }
