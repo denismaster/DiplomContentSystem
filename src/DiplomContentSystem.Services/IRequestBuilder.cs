e46fd6e52a09c84888a2cfc8652f2f7c072bd030
denismaster <denismaster@outlook.com>
2017-03-19 00:51:13 +0300

    DCS-19: Implemented RequestBuilder

---
+++ src/DiplomContentSystem.Services/IRequestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using DiplomContentSystem.Core;
+
+namespace DiplomContentSystem.Services
+{
+    public interface IRequestBuilder<T> where T : class, IEntity
+    {
+        RequestBuilder<T> UseDto(Dto.Request requestDto);
+        RequestBuilder<T> UseFilters();
+        RequestBuilder<T> UsePaging();
+        RequestBuilder<T> UsePaging(int page, int pageSize);
+        RequestBuilder<T> UseSortings(string defaultSorting);
+        Request<T> Build();
+    }
+}
--- src/DiplomContentSystem.Services/RequestBuilder.cs
+++ src/DiplomContentSystem.Services/RequestBuilder.cs
@@ -1,24 +1,48 @@
 using System;
 using System.Linq.Expressions;
+using DiplomContentSystem.Core;
+using DiplomContentSystem.Dto;
 
 namespace DiplomContentSystem.Services
 {
-    //TODO REDONE WITH BUILDER PATTERN.
-    public class RequestBuilder<T> where T : class, Core.IEntity
+    public abstract class RequestBuilder<T> : IRequestBuilder<T> where T : class, Core.IEntity
     {
-        private readonly Func<string, Expression<Func<T, object>>> SortingBuilder;
-        private readonly Func<string, Expression<Func<T, object>>> FilterBuilder;
-        public RequestBuilder<T> UseFilters()
+        protected Dto.Request _requestDto;
+        protected Request<T> _dbRequest;
+        public RequestBuilder()
         {
-            return null;
+            _dbRequest = new Request<T>();
         }
-        public RequestBuilder<T> UseSortings()
+        public RequestBuilder<T> UseDto(Request requestDto)
         {
-            return null;
+            if (requestDto == null) throw new ArgumentNullException(nameof(requestDto));
+            _requestDto = requestDto;
+            return this;
+        }
+        public virtual RequestBuilder<T> UseFilters()
+        {
+            return this;
+        }
+        public virtual RequestBuilder<T> UseSortings(string defaultSorting)
+        {
+            return this;
         }
         public RequestBuilder<T> UsePaging()
         {
-            return null;
+            _dbRequest.Skip = _requestDto.Skip;
+            _dbRequest.Take = _requestDto.Take;
+            return this;
         }
+        public RequestBuilder<T> UsePaging(int skip, int take)
+        {
+            _dbRequest.Skip = skip;
+            _dbRequest.Take = take;
+            return this;
+        }
+
+        public Request<T> Build()
+        {
+            return _dbRequest;
+        }        
     }
 }
