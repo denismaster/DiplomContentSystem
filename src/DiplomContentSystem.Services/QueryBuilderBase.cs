0f3d5b4bb44649bbdab7f4ce336c661ba6a66897
denismaster <denismaster@outlook.com>
2017-03-19 21:00:21 +0300

    DCS-19: Using new filenames instead of old.

--- src/DiplomContentSystem.Services/QueryBuilderBase.cs
+++ src/DiplomContentSystem.Services/QueryBuilderBase.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using DiplomContentSystem.Core;
 using DiplomContentSystem.Dto;
 
 namespace DiplomContentSystem.Services
 {
-    public abstract class QueryBuilder<T> : IQueryBuilder<T> where T : class, Core.IEntity
+    public abstract class QueryBuilderBase<T> : IQueryBuilder<T> where T : class, Core.IEntity
     {
         protected Dto.Request _requestDto;
         protected Query<T> _dbQuery;
-        public QueryBuilder()
+        public QueryBuilderBase()
         {
             _dbQuery = new Query<T>();
         }
@@ -25,8 +26,23 @@ namespace DiplomContentSystem.Services
         }
         public virtual IQueryBuilder<T> UseSortings(string defaultSorting)
         {
+            _dbQuery.SortExpressions = new System.Collections.Generic.List<SortExpression<T>>();
+            if (_requestDto.Sortings == null || !_requestDto.Sortings.Any())
+            {
+                _dbQuery.SortExpressions.Add(new SortExpression<T>(GetSortExpression(defaultSorting), SortDirection.Ascending));
+            }
+            else
+            {
+                foreach (var sorting in _requestDto.Sortings)
+                {
+                    _dbQuery.SortExpressions.Add(new SortExpression<T>(GetSortExpression(sorting.FieldName), (SortDirection)sorting.Direction));
+                }
+            }
             return this;
         }
+
+        protected abstract Expression<Func<T, object>> GetSortExpression(string sortParamName);
+
         public IQueryBuilder<T> UsePaging()
         {
             _dbQuery.Skip = _requestDto.Skip;
---
+++ src/DiplomContentSystem.Services/Teachers/TeachersQueryBuilder.cs
@@ -0,0 +1,29 @@
+﻿using DiplomContentSystem.Core;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+namespace DiplomContentSystem.Services.Teachers
+{
+    public class TeachersQueryBuilder : QueryBuilderBase<Teacher>
+    {
+        protected override Expression<Func<Teacher, object>> GetSortExpression(string sortFieldName)
+        {
+            switch (sortFieldName)
+            {
+                case "fio": return teacher => teacher.FIO;
+                case "position": return teacher => teacher.Position.Name;
+                case "maxWorkCount": return teacher => teacher.MaxWorkCount;
+                default: return teacher => teacher.Id;
+            }
+        }
+        public override IQueryBuilder<Teacher> UseFilters()
+        {
+            var teachersRequest = _requestDto as Dto.TeacherRequest;
+            if (!string.IsNullOrEmpty(teachersRequest.FIO))
+            {
+                _dbQuery.FilterExpression = teacher => teacher.FIO.Contains(teachersRequest.FIO);
+            }
+            return this;
+        }
+    }
+}
