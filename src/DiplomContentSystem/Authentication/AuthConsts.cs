ad4b8c0504a56c4f652a763c26e52ccbbb7f9f9b
denismaster <denismaster@outlook.com>
2017-03-21 00:31:57 +0300

    DCS-12: Replace AuthConsts to separate file.

---
+++ src/DiplomContentSystem/Authentication/AuthConsts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiplomContentSystem.Authentication
+{
+    public class AuthConsts
+    {
+        public const string ClaimUserType = "UserType";
+        public const string PolicyAdmin = "AdminPolicy";
+        public const string PolicyUser = "UserPolicy";
+        public const string RoleUser = "User";
+        public const string RoleAdmin = "Admin";
+    }
+}
