f33f7d344bef9f21dc9bab293aa555bda6b6e2b1
denismaster <denismaster@outlook.com>
2017-03-21 11:24:12 +0300

    DCS-24: Add CodeMirror to textarea in our page

--- src/DiplomContentSystem/ClientApp/app/templating/components/new-template.component.ts
+++ src/DiplomContentSystem/ClientApp/app/templating/components/new-template.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as _codeMirror from "codemirror";
+import  "codemirror/mode/stex/stex"
 
 @Component({
     selector: 'new-template',
     templateUrl: './new-template.component.html',
-    styles:[require("./new-template.component.css")]
+    styles: [require("./new-template.component.css")]
 })
-export class NewTemplateComponent {
+export class NewTemplateComponent implements AfterViewInit {
     public currentCount = 0;
     public templateText:string;
 
     public incrementCounter() {
         this.currentCount++;
+    }
+
+    ngAfterViewInit() {
+        const textArea = document.querySelector("#code-area") as HTMLTextAreaElement;
+        _codeMirror.fromTextArea(textArea,{
+            lineNumbers: true,
+            mode: "stex",
+            theme: 'default'
+        });
     }
 }
