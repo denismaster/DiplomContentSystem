285e600319c9fce66829e32f925fd9f66846ea0c
Denis Obolensky <denis_obolensky@xrm.ru>
2017-03-25 13:23:45 +0300

    DCS-12: Add AuthorizeFilter to our Controllers.

--- src/DiplomContentSystem/Controllers/TeacherController.cs
+++ src/DiplomContentSystem/Controllers/TeacherController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using DiplomContentSystem.Services.Teachers;
+using DiplomContentSystem.Authentication;
 using DiplomContentSystem.Core;
 using Newtonsoft.Json;
 namespace DiplomContentSystem.Controllers
 {   
     [Route("api/[controller]")]
+    [Authorize(Policy=AuthConsts.PolicyUser)]
     public class TeachersController : Controller
     {
         private readonly TeacherService _service;
