7c2196d16925aeeba5a8f53990b46acadfcb52f9
Denis Obolensky <denismaster@outlook.com>
2017-04-19 02:06:48 +0300

    DCS-30: Changes to MappingProfile to use modifiable speciality.

--- src/DiplomContentSystem.Services/MappingProfile.cs
+++ src/DiplomContentSystem.Services/MappingProfile.cs
@@ -14,10 +14,11 @@ namespace DiplomContentSystem.Services
             CreateMap<Teacher, TeacherListItem>()
                 .ForMember(item => item.Speciality, opt => opt.MapFrom(src => (src.Speciality != null) ? src.Speciality.ShortName : null))
                 .ForMember(item => item.Position, opt => opt.MapFrom(src => (src.Position != null) ? src.Position.Name : null));
+                
             CreateMap<TeacherEditItem, Teacher>()
                 .ForMember(item => item.Id, opt => opt.MapFrom(src => src.Id == null ? 0 : src.Id))
                 .ForMember(item => item.PositionId, opt => opt.MapFrom(src => src.PositionId))
-                .ForMember(item => item.SpecialityId, opt => opt.UseValue(1))
+                .ForMember(item => item.SpecialityId, opt => opt.MapFrom(src=>src.SpecialityId))
                 .ForMember(item => item.Position, opt => opt.Ignore())
                 .ForMember(item => item.Speciality, opt => opt.Ignore());
 
