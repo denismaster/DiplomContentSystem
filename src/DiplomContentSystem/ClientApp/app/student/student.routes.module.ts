ac64722c34a9b1aae263a174f7647c7d820b800a
Denis Obolensky <denismaster@outlook.com>
2017-04-19 08:18:47 +0300

    DCS-30: First part of student add.

--- src/DiplomContentSystem/ClientApp/app/student/student.routes.module.ts
+++ src/DiplomContentSystem/ClientApp/app/student/student.routes.module.ts
@@ -1,17 +1,18 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentComponent } from './components/student.component';
+import { StudentAddComponent } from './components/add.component';
 
 const routes: Routes = [
     {
         path: 'students',
         component: StudentComponent,
     },
-    /*{
-        path: 'students/new',
-        component: TeachersAddComponent,
-    },
     {
+        path: 'students/new',
+        component: StudentAddComponent,
+    },
+    /*{
         path: 'students/edit/:id',
         component: TeachersEditComponent,
     }*/
