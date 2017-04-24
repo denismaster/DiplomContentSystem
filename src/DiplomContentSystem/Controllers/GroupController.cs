a2d344eb02c6e573f8533db0c4f1bdc4825b0885
denismaster <denismaster@outlook.com>
2017-04-24 08:35:38 +0300

    DCS-30: Changes to GroupController.

--- src/DiplomContentSystem/Controllers/GroupController.cs
+++ src/DiplomContentSystem/Controllers/GroupController.cs
@@ -5,26 +5,30 @@ using Microsoft.AspNetCore.Authorization;
 using DiplomContentSystem.Authentication;
 using DiplomContentSystem.Core;
 using DiplomContentSystem.Dto;
+using DiplomContentSystem.Services.Groups;
 namespace DiplomContentSystem.Controllers
 {
     [Route("api/groups")]
     [Authorize(Policy=AuthConsts.PolicyUser)]
     public class GroupController : Controller
     {
-        private readonly IRepository<Group> _repository;
-        public GroupController(IRepository<Group> repository)
+        private readonly GroupService _service;
+        public GroupController(GroupService service)
         {
-            if(repository==null) throw new ArgumentNullException(nameof(repository));
-            _repository = repository;
+            if(service==null) throw new ArgumentNullException(nameof(service));
+            _service = service;
         }
+
+        public IActionResult Get([FromBody] Dto.GroupRequest request )
+        {
+            return Ok(_service.GetGroups(request));
+        }
+
 
         [HttpGet("select-list")]
         public IActionResult Get()
         {
-            var result = _repository.Get().Select(item=>new SelectListItem(){
-                Value = item.Id.ToString(),
-                Text = item.Name
-            });
+            var result = _service.GetAsSelectList();
             if(result!=null) return Ok(result);
             return BadRequest();
         }
