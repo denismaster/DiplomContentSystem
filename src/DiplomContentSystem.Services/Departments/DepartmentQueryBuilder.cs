20ea4343927f290fa67e14165aae051c962c9877
denismaster <denismaster@outlook.com>
2017-05-03 21:27:00 +0300

    DCS-30:Service and QueryBuilder for Departments.

---
+++ src/DiplomContentSystem.Services/Departments/DepartmentQueryBuilder.cs
@@ -0,0 +1,32 @@
+﻿using DiplomContentSystem.Core;
+using DiplomContentSystem.Services;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+namespace DiplomContentSystem.Services.Departments
+{
+    public class DepartmentsQueryBuilder : QueryBuilderBase<Department>
+    {
+        protected override Expression<Func<Department, object>> GetSortExpression(string sortFieldName)
+        {
+            switch (sortFieldName)
+            {
+                case "name": return department => department.Name;
+                case "specialities": return department => department.Specialities.Count;
+                default: return department => department.Id;
+            }
+        }
+
+        public override IQueryBuilder<Department> UseFilters()
+        {
+            var departmentsRequest = _requestDto as Dto.DepartmentRequest;
+            var expression  = Services.PredicateBuilder.True<Department>();
+            if (!string.IsNullOrEmpty(departmentsRequest.Name))
+            {
+               expression =  expression.And(department => department.Name.Contains(departmentsRequest.Name));
+            }
+            _dbQuery.FilterExpression = expression;
+            return this;
+        }
+    }
+}
