6b843185230c30a9b546e12b840b6c0f303b4f2c
denismaster <denismaster@outlook.com>
2017-05-03 21:22:55 +0300

    DCS-30: We now show current WorkCount for teachers

--- src/DiplomContentSystem.Dto/Teachers/TeacherListItem.cs
+++ src/DiplomContentSystem.Dto/Teachers/TeacherListItem.cs
@@ -6,6 +6,7 @@ namespace DiplomContentSystem.Dto
         public string FIO {get;set;}
         public string Position {get;set;}
         public string Department {get;set;}
+        public int WorkCount {get;set;}
         public int MaxWorkCount {get;set;}
     }
 }
