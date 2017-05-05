4fbc79021ef6e684b20c1549dd63101c923c9917
Denis Obolensky <denis_obolensky@xrm.ru>
2017-05-05 17:35:05 +0300

    DCS-30: Add CalendarEvents services.

---
+++ src/DiplomContentSystem.Services/CalendarEvents/CalendarEventService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DiplomContentSystem.Core;
+using DiplomContentSystem.Dto;
+using AutoMapper;
+namespace DiplomContentSystem.Services.CalendarEvents
+{
+    public class CalendarEventService
+    {
+        private readonly IRepository<CalendarEvent> _repository;
+        private readonly IMapper _mapper;
+
+        public CalendarEventService(IRepository<CalendarEvent> repository, IMapper mapper)
+        {
+            if (repository == null) throw new ArgumentNullException(nameof(repository));
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            _repository = repository;
+            _mapper = mapper;
+            
+        }
+
+        public Dto.ListResponse<CalendarEventListItem> GetCalendarEvents(CalendarEventRequest request)
+        {
+            var queryBuilder = new CalendarEventsQueryBuilder();
+            var response = new ListResponse<CalendarEventListItem>();
+            string[] includes = {"Speciality", "Speciality.Department","Students"};
+
+            var query = queryBuilder.UseDto(request)
+                                    .UsePaging()
+                                    .UseFilters()
+                                    .UseSortings(defaultSorting:"id")
+                                    .Build();
+
+            response.TotalCount = _repository.Count(query.FilterExpression);
+            response.Items = _mapper.Map<IEnumerable<CalendarEventListItem>>(_repository.Get(query, includes));
+
+            return response;
+        }
+
+        public CalendarEvent Get(int id)
+        {   
+            string[] includes = {"Speciality","Speciality.Department","Students"};
+            var result = _repository.Get(id, includes);
+            result.Speciality.Department.Specialities = null;
+            return result;
+        }
+
+        public IEnumerable<SelectListItem> GetAsSelectList()
+        {
+            return this._repository.Get().Select(item=>new SelectListItem(){
+                Value = item.Id.ToString(),
+                Text = item.Name
+            });
+        }
+
+         public bool Add(CalendarEventEditItem CalendarEventDto)
+        {
+            var dbCalendarEvent = _mapper.Map<CalendarEvent>(CalendarEventDto);
+            _repository.Add(dbCalendarEvent);
+            _repository.SaveChanges();
+            return true;
+        }
+
+        public bool Update(CalendarEventEditItem CalendarEventDto)
+        {
+            var dbCalendarEvent = _mapper.Map<CalendarEvent>(CalendarEventDto);
+            _repository.Update(dbCalendarEvent);
+            _repository.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ src/DiplomContentSystem.Services/CalendarEvents/CalendarEventsQueryBuilder.cs
@@ -0,0 +1,37 @@
+﻿using DiplomContentSystem.Core;
+using DiplomContentSystem.Services;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+namespace DiplomContentSystem.Services.CalendarEvents
+{
+    public class CalendarEventsQueryBuilder : QueryBuilderBase<CalendarEvent>
+    {
+        protected override Expression<Func<CalendarEvent, object>> GetSortExpression(string sortFieldName)
+        {
+            switch (sortFieldName)
+            {
+                case "name": return event => event.Name;
+                case "speciality": return event => event.Speciality.Name;
+                case "department": return event => event.Speciality.Department.Name;
+                default: return event => event.Id;
+            }
+        }
+
+        public override IQueryBuilder<CalendarEvent> UseFilters()
+        {
+            var eventsRequest = _requestDto as Dto.CalendarEventRequest;
+            var expression  = Services.PredicateBuilder.True<CalendarEvent>();
+            if (!string.IsNullOrEmpty(eventsRequest.Name))
+            {
+               expression =  expression.And(event => event.Name.Contains(eventsRequest.Name));
+            }
+            if (eventsRequest.Department > 0)
+            {
+               expression =  expression.And(event => event.Speciality.Department.Id == eventsRequest.Department);
+            }
+            _dbQuery.FilterExpression = expression;
+            return this;
+        }
+    }
+}
