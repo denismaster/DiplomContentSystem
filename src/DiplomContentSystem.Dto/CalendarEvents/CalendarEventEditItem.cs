dbb27ff384d38572aa5440bb1bd834efa89c42ab
Denis Obolensky <denis_obolensky@xrm.ru>
2017-05-05 17:34:40 +0300

    DCS-30: Add CalendarEventDto's

---
+++ src/DiplomContentSystem.Dto/CalendarEvents/CalendarEventEditItem.cs
@@ -0,0 +1,9 @@
+namespace DiplomContentSystem.Dto
+{
+    public class CalendarEventEditItem
+    {
+        public int? Id {get;set;}
+        public string Name {get;set;}
+        public int SpecialityId {get;set;}
+    }
+}
