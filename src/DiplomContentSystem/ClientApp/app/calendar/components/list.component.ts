3b62061570c90ecb6a5f9497cf3d5f7e3870164f
Denis Obolensky <denis_obolensky@xrm.ru>
2017-05-05 17:36:03 +0300

    DCS-30: Add basic CalendarEvent components.

---
+++ src/DiplomContentSystem/ClientApp/app/calendar/components/list.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { RtList, filter } from 'right-angled';
+import { CalendarEvent } from '../models/calendar';
+import { CalendarService } from '../calendar.service';
+import { SelectListItem } from '../../shared/select-list-item';
+import { TeacherService } from '../../teacher/teacher.service';
+@Component({
+    selector: 'calendar-list',
+    templateUrl: './list.component.html'
+})
+export class CalendarsComponent {
+
+    @filter()
+    private name: string;
+
+    @filter()
+    private department: number = 0;
+
+    private showFilter: boolean;
+    private isLoading: boolean = true;
+
+    private departmentOptions: SelectListItem[];
+
+    constructor(private service: CalendarService, private teacherService: TeacherService) {
+        this.teacherService.getDepartments().subscribe(r => this.departmentOptions = r);
+    }
+
+    getList = (request) => {
+        this.isLoading = true;
+        return this.service.getList(request).do((response) => { if (response.totalCount > 0) this.isLoading = false });
+    }
+
+    onListInit(list: RtList): void {
+        list.registerFilterTarget(this);
+    }
+}
+
