071c4c417895022ece8f6491df0c39e47edef91e
denismaster <denismaster@outlook.com>
2017-05-06 11:29:18 +0300

    DCS-30: Some changes to template of calendar plan.

--- src/DiplomContentSystem/ClientApp/app/calendar/components/list.component.ts
+++ src/DiplomContentSystem/ClientApp/app/calendar/components/list.component.ts
@@ -29,48 +29,57 @@ export class CalendarsComponent {
     getList = () => {
         return Promise.resolve([
                 {
+                    id:1,
                     name:"Анализ предметной области",
                     startDate: new Date(2016,1,1),
                     endDate: new Date(2016,1,30),
                     checked:true
                 },
                 {
+                    id:2,
                     name:"Проектирование информационной системы",
                     startDate: new Date(2016,2,1),
                     endDate: new Date(2016,2,28),
                     checked:true
                 },
                 {
+                    id:3,
                     name:"Проектирование информационной базы данных системы",
                     startDate: new Date(2016,3,1),
                     endDate: new Date(2016,4,30),checked:true
                 },
                 {
+                    id:4,
                     name:"Физическая реализация системы",
                     startDate: new Date(2016,5,1),
                     endDate: new Date(2016,7,30)
                 },
                 {
+                    id:5,
                     name:"Оформление пояснительной записки",
                     startDate: new Date(2016,8,1),
                     endDate: new Date(2016,8,30)
                 },
                 {
+                    id:6,
                     name:"Нормоконтроль",
                     startDate: new Date(2016,9,1),
                     endDate: new Date(2016,9,4)
                 },
                 {
+                    id:7,
                     name:"Антиплагиат",
                     startDate: new Date(2016,9,5),
                     endDate: new Date(2016,9,14)
                 },
                 {
+                    id:8,
                     name:"Предзащита",
                     startDate: new Date(2016,9,15),
                     endDate: new Date(2016,9,15)
                 },
                 {
+                    id:9,
                     name:"Защита дипломной работы",
                     startDate: new Date(2016,10,7)
                 }
