d31b153a26f275f024d117906a15c04a65f40526
denismaster <denismaster@outlook.com>
2017-05-06 14:05:56 +0300

    DCS-30: Upate CalendarEventListItem DTO

--- src/DiplomContentSystem.Dto/CalendarEvents/CalendarEventListItem.cs
+++ src/DiplomContentSystem.Dto/CalendarEvents/CalendarEventListItem.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace DiplomContentSystem.Dto
 {
     public class CalendarEventListItem
     {
         public int Id {get;set;}
         public string Name {get;set;}
-        public string Speciality {get;set;}
-        public string Department {get;set;}
-        public int StudentsCount {get;set;}
+        public DateTime StartDate {get;set;}
+        public DateTime EndDate {get;set;}
+        public bool Accepted {get;set;}
     }
 }
