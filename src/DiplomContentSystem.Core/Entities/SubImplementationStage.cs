fb3070e1b998fe947828e6f9cad0d63f1ccf5190
denismaster <denismaster@outlook.com>
2017-05-06 11:26:00 +0300

    DCS-30: Redone ImplementationStage and CustomStage.

--- src/DiplomContentSystem.Core/Entities/SubImplementationStage.cs
+++ src/DiplomContentSystem.Core/Entities/SubImplementationStage.cs
@@ -5,16 +5,15 @@ using System.Threading.Tasks;
 
 namespace DiplomContentSystem.Core
 {
-    public class SubImplementationStage : IEntity
+    public class CustomStage : IEntity, IStage
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
-
-        public int ImplementationStageId { get; set; }
-        public ImplementationStage ImplementationStage { get; set; }
-
-        public List<SubImplementationStageComment> SubImplementationStageComment { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int DiplomWorkId { get; set; }
+        public DiplomWork DiplomWork { get; set; }
+        public bool Accepted {get;set;}
     }
 }
