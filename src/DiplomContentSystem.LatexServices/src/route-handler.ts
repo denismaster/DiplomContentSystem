f8be5e15fa92ca7b52e04b7147626e63f1b49841
denismaster <denismaster@outlook.com>
2017-05-08 08:40:29 +0300

    DCS-30: Now use request data instead of predefined in code

--- src/DiplomContentSystem.LatexServices/src/route-handler.ts
+++ src/DiplomContentSystem.LatexServices/src/route-handler.ts
@@ -32,63 +32,31 @@ export class RouteHandler {
     public docx(request: Request, response: Response): void {
         try {
 
-            //Load the docx file as a binary
-            console.log("init convert docx");
             let content = fs
-                .readFileSync(path.resolve(__dirname,"..","templates", "docx", 'calendar.docx'), 'binary');
-                console.log("");
-            console.log(content);
+                .readFileSync(path.resolve(__dirname, "..", "templates", "docx", 'calendar.docx'), 'binary');
+            //console.log(content);
             let zip = new JSZip(content);
             let doc = new Docxtemplater();
             doc.loadZip(zip);
-            const data = {
-                stages: [
-                    {
-                        id: 1,
-                        name: "Первый этап",
-                        startDate: 1,
-                        endDate: 1
-                    },
-                    {
-                        id: 2,
-                        name: "Второй этап",
-                        startDate: 3,
-                        endDate: 2
-                    },
-                    {
-                        id: 3,
-                        name: "Третий этап",
-                        startDate: 2,
-                        endDate: 3
-                    }
-                ],
-                teacher: "Иванов И.А",
-                student: "Юндин А.В."
-            }
-            //const data = JSON.parse(request.body.data.toString());
+            const data = request.body;
             doc.setData(data);
+            console.log(data.stages[0].startDate instanceof Date);
             try {
-                console.log("now render");
-                // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
                 doc.render()
             }
             catch (error) {
-                console.log("render error");
-                var e = {
+                console.error("render error");
+                let e = {
                     message: error.message,
                     name: error.name,
                     stack: error.stack,
                     properties: error.properties,
                 }
                 console.log(JSON.stringify({ error: e }));
-                // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
                 throw error;
             }
             let buf = doc.getZip()
                 .generate({ type: 'nodebuffer' }) as Buffer;
-
-            console.log(buf);
-            // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
             response.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
             response.setHeader("Content-Disposition", "attachment; filename=\"calendar.docx\"");
             response.setHeader("Content-Length", buf.length.toString());
