97faeb4106f3a2f651c542c58396bc6db72410d1
denismaster <denismaster@outlook.com>
2017-06-09 08:27:01 +0300

    DCS-30: Add migrations for templates

--- src/DiplomContentSystem.DataLayer/Migrations/20170608215909_AddTemplates.Designer.cs
+++ src/DiplomContentSystem.DataLayer/Migrations/20170608215909_AddTemplates.Designer.cs
@@ -8,9 +8,10 @@ using DiplomContentSystem.DataLayer;
 namespace DiplomContentSystem.DataLayer.Migrations
 {
     [DbContext(typeof(DiplomContext))]
-    partial class DiplomContextModelSnapshot : ModelSnapshot
+    [Migration("20170608215909_AddTemplates")]
+    partial class AddTemplates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
---
+++ src/DiplomContentSystem.DataLayer/Migrations/20170608215909_AddTemplates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DiplomContentSystem.DataLayer.Migrations
+{
+    public partial class AddTemplates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TemplateType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemplateType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Template",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    FileName = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    TemplateTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Template", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Template_TemplateType_TemplateTypeId",
+                        column: x => x.TemplateTypeId,
+                        principalTable: "TemplateType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Template_TemplateTypeId",
+                table: "Template",
+                column: "TemplateTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Template");
+
+            migrationBuilder.DropTable(
+                name: "TemplateType");
+        }
+    }
+}
