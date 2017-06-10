fdb4cb50e9e5f7165bac8aa4e26cde6c2fbfadad
denismaster <denismaster@outlook.com>
2017-06-10 18:29:01 +0300

    DCS-24: Add commentService

---
+++ src/DiplomContentSystem/ClientApp/app/shared/comments/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ApiService, ApiRoutes } from '../api-service';
+import { DataService } from '../data-service';
+import { Observable } from 'rxjs/Observable';
+import { Comment } from './comment';
+
+@Injectable()
+export class CommentService extends ApiService<Comment>{
+    protected static routes:ApiRoutes = 
+    {
+        getUrl:"/api/comments",
+        addUrl:"api/diploms/add",
+    }
+    constructor(http: DataService)
+    {
+        super(CommentService.routes,http);
+    }
+}
